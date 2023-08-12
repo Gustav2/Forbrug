1d2feeda1003bb8a8c220855ea20dbcc874897dc
Gustav <gustav@gustavnybro.com>
2023-08-12 20:27:25 +0200

    added method to get usage from date

--- energinet.py
+++ energinet.py
@@ -2,6 +2,7 @@ import json
 import requests
 import datetime
 
+
 class Energinet:
     endpoint = "https://api.eloverblik.dk/customerapi/api"
     user_token = ""
@@ -22,6 +23,25 @@ class Energinet:
         json_data = json.loads(req.text)
         return json_data["result"][0]["meteringPointId"]
 
+    def get_usage_from_date(self, date: datetime.datetime):
+        next_day = date + datetime.timedelta(days=1)
+        req = requests.post(
+            self.endpoint + f"/meterdata/gettimeseries/{date.strftime('%Y-%m-%d')}/{next_day.strftime('%Y-%m-%d')}/Hour",
+            json={
+                "meteringPoints": {
+                    "meteringPoint": [
+                        self.get_metering_point_id()
+                    ]
+                }
+            },
+            headers=self.headers)
+        json_data = json.loads(req.text)["result"][0]["MyEnergyData_MarketDocument"]["TimeSeries"][0]["Period"][0]["Point"]
+
+        price_data = []
+        for amount in json_data:
+            price_data.append(float(amount["out_Quantity.quantity"]))
+        return price_data
+
     def __init__(self, user_token):
         self.user_token = user_token
         self.refresh_token = self.get_refresh_token()
@@ -29,4 +49,3 @@ class Energinet:
             "Authorization": f"Bearer {self.refresh_token}",
             "accept": "application/json"
         }
-
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
 import json
 import os
-import time
+import datetime
 from energinet import Energinet
 from norlys import Norlys
 
 
 if __name__ == "__main__":
     energinet = Energinet(os.environ["ENERGINET_TOKEN"])
-    print(energinet.get_metering_point_id())
+    print(energinet.get_usage_from_date(datetime.datetime(2023, 7, 10)))
+
+    norlys = Norlys()
+    print(norlys.get_data_from_date(datetime.datetime(2023, 7, 10)))
+
