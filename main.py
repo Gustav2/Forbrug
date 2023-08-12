6e9a7dbf9633a7f5e18778dc3da201eb8f59c4a6
Gustav <gustav@gustavnybro.com>
2023-08-12 20:46:08 +0200

    added totals and now working

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import datetime
 from energinet import Energinet
 from norlys import Norlys
 
-
 if __name__ == "__main__":
     energinet = Energinet(os.environ["ENERGINET_TOKEN"])
-    print(energinet.get_usage_from_date(datetime.datetime(2023, 7, 10)))
+    energinet_usage = energinet.get_usage_from_date(datetime.datetime(2023, 8, 1))
 
     norlys = Norlys()
-    print(norlys.get_data_from_date(datetime.datetime(2023, 7, 10)))
+    norlys_prices = norlys.get_data_from_date(datetime.datetime(2023, 8, 1), add_fees=True)
 
+    total_prices = []
+    for i in range(len(energinet_usage)):
+        total_prices.append(energinet_usage[i] * norlys_prices[i])
+
+    print("Total pris", round(sum(total_prices)/100, 3), "kr")
+    print("Total energi", sum(energinet_usage), "kWh")
--- norlys.py
+++ norlys.py
@@ -4,22 +4,35 @@ import datetime
 
 
 class Norlys:
-    endpoint = "https://norlys.dk/api/flexel/getall"
+    ENDPOINT = "https://norlys.dk/api/flexel/getall"
+    NET_FEE = 0
+    ENERGY_FEE = 14
+    STATE_FEE = 87
+
+    def add_fees(self, prices):
+        new_prices = []
+        for price in prices:
+            new_prices.append(price + self.NET_FEE + self.ENERGY_FEE + self.STATE_FEE)
+        return new_prices
 
     def get_data(self):
-        req = requests.get(self.endpoint)
+        req = requests.get(self.ENDPOINT)
         json_data = json.loads(req.text)
         return json_data
 
-    def get_data_from_date(self, date: datetime.datetime):
-        req = requests.get(self.endpoint)
+    def get_data_from_date(self, date: datetime.datetime, add_fees=True):
+        req = requests.get(self.ENDPOINT)
         json_data = json.loads(req.text)
 
         for data in json_data:
             price_date = datetime.datetime.strptime(data["PriceDate"], "%Y-%m-%dT%H:%M:%SZ")
             if price_date == date:
-                return data
-
+                prices = []
+                for price in data["DisplayPrices"]:
+                    prices.append(price["value"])
+                if add_fees:
+                    return self.add_fees(prices)
+                return prices
 
 
 if __name__ == "__main__":
