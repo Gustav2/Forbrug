1692bfa362e8cfdf6cd81f6c5d960a070a126d10
Gustav <gustav@gustavnybro.com>
2023-08-12 20:53:01 +0200

    added useability

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from norlys import Norlys
 
 if __name__ == "__main__":
     energinet = Energinet(os.environ["ENERGINET_TOKEN"])
-    energinet_usage = energinet.get_usage_from_date(datetime.datetime(2023, 8, 1))
-
     norlys = Norlys()
-    norlys_prices = norlys.get_data_from_date(datetime.datetime(2023, 8, 1), add_fees=True)
+    date = datetime.datetime(2023, 8, 7)
+
+    energinet_usage = energinet.get_usage_from_date(date)
+    norlys_prices = norlys.get_data_from_date(date, add_fees=True)
 
     total_prices = []
     for i in range(len(energinet_usage)):
         total_prices.append(energinet_usage[i] * norlys_prices[i])
 
     print("Total pris", round(sum(total_prices)/100, 3), "kr")
-    print("Total energi", sum(energinet_usage), "kWh")
+    print("Total energi", round(sum(energinet_usage), 3), "kWh")
